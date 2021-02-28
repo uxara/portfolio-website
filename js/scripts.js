08103a97a8d72e9e89ff0f96034ec3b026293d76
uxara <77619870+uxara@users.noreply.github.com>
2021-02-28 18:39:52 +0100

    Add javascript to menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Welcome Dear Guest! This Is My Portfolio Site!")
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
