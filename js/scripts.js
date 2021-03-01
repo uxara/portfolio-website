1d684316985c91f9b4c96a3fbc9be86afda94e04
uxara <77619870+uxara@users.noreply.github.com>
2021-03-02 00:36:32 +0100

    Add cards to index

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,22 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+//for image slider
+
+//$(document).ready(function sliderdonkey(){
+//  x = 0;
+  //for next slider
+//  $('.btn-next').click(function sliderdonkey(){
+  //  x = (x<=300)?(x+100):0;   //es6
+//    $('figure').css('left', -x+"%");
+
+//  });
+//for previous slider
+
+//$('.btn-prev').click(function sliderdonkey(){
+//  x = (x>=100)?(x-100):400;   //es6
+//  $('figure').css('left', -x+"%");
+
+//});
+//}
